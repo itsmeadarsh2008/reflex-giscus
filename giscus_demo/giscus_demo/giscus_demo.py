7ce05e6bfce02a03f625e7e98da24868c23e11e8
Adarsh Gourab Mahalik <gourabmahalikadarsh@gmail.com>
2024-08-06 22:42:25 +0530

    Made the website responsive.

--- giscus_demo/giscus_demo/giscus_demo.py
+++ giscus_demo/giscus_demo/giscus_demo.py
@@ -1,42 +1,30 @@
 """Welcome to Reflex! This file showcases the custom component in a basic app."""
 
-from rxconfig import config
+# from rxconfig import config
 import reflex as rx
 from reflex_giscus import giscus
-filename = f"{config.app_name}/{config.app_name}.py"
-
-
-class State(rx.State):
-    """The app state."""
-
-    pass
 
 def index() -> rx.Component:
     return rx.center(
         # rx.theme_panel(),
         rx.vstack(
-        # render_document(),
-        rx.spacer(),
-        rx.heading('''Welcome to Reflex Giscus!''', size='9', align='center'),
-        rx.link('Generate your config at giscus.vercel.app',href='https://giscus.vercel.app/', align='center'),
-        rx.code_block(
-            '''from reflex_giscus import giscus
-
-rx.color_mode_cond(
-    light=giscus(id="comments", theme="light"), dark=giscus(id="comments", theme="dark")
-)''',language='python'),
-        rx.switch(on_change=rx.toggle_color_mode),
+            # render_document(),
+            rx.spacer(),
+            rx.heading("""Welcome to Reflex Giscus!""", size="9", align="center"),
+            rx.switch(on_change=rx.toggle_color_mode),
             rx.text(
-                "The missing commenting system from Reflex 🚀",
+                "The missing commenting system from Reflex 🧐",
                 font_size="2em",
+                align="center",
             ),
             rx.color_mode_cond(
-                light=giscus(id='comments', theme='light'),
-                dark=giscus(id='comments', theme='dark'),
+                light=giscus(id="comments", theme="light", align="center"),
+                dark=giscus(id="comments", theme="dark", align="center")
             ),
             rx.logo(),
             align="center",
             spacing="7",
+            padding="30px",
         ),
         # height="100vh",
     )
